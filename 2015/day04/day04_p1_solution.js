1f702c21bde3d5b7de29e976cda66ad90635347e
dovkh42 <doveloper42@gmail.com>
2024-07-27 22:10:19 +0300

    day4 solution

---
+++ 2015/day04/day04_p1_solution.js
@@ -0,0 +1,33 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import readline from 'readline';
+
+
+const filePath = './day04_input.txt';
+const fileStream = fs.createReadStream(filePath);
+
+
+const generateMD5Hash = (data) => {
+    return crypto.createHash('md5').update(data).digest('hex');
+}
+
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+
+let secret;
+
+rl.on('line', (line) => {
+    secret = line;
+});
+
+rl.on('close', () => {
+    console.log(`Secret is: ${secret}`);
+    let i = 0;
+    for (; !generateMD5Hash(secret + i.toString()).startsWith('00000'); ++i) {};
+    console.log(`Answer is: ${i}`);
+});
+
+console.log('Reading file asynchronously...');
