5dd60d3f3b14854e2b43f18150b89f1fcfa4b499
dovkh42 <doveloper42@gmail.com>
2024-07-27 14:05:38 +0300

    eslint rule disabled

--- aoc2015/eslint.config.js
+++ aoc2015/eslint.config.js
@@ -24,7 +24,7 @@ export default [
         rules: {
             'simple-import-sort/imports': "error",
             'simple-import-sort/exports': 'error',
-
+            'security/detect-object-injection': 'off'
         }
     }
 ];
