7fbef1675579cd06fe9341388d3239f58f46a0ba
dovkh42 <doveloper42@gmail.com>
2024-07-29 22:29:12 +0300

    bug fix

--- 2016/day03/day03_p1_solution.js
+++ 2016/day03/day03_p1_solution.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 const filePath = './day03_input.txt';
 const data = fs.readFileSync(filePath, 'utf8');
 
-const sideLengthSets = data.split('\r\n').map((line)=> line.trim().split('  ').map(Number));
+const sideLengthSets = data.split('\r\n').map((line) => line.trim().split(/\s+/).map(Number));
 const numOfValidTriangles = sideLengthSets.filter(([a, b, c]) => a + b > c && b + c > a && a + c > b).length;
 
 console.log(`Answer is: ${numOfValidTriangles}`);
