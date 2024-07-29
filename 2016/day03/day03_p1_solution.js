ceb37e5c7fe3234334358291745927fd597336ad
dovkh42 <doveloper42@gmail.com>
2024-07-29 22:19:01 +0300

    refactored for better complexity

--- 2016/day03/day03_p1_solution.js
+++ 2016/day03/day03_p1_solution.js
@@ -4,16 +4,6 @@ const filePath = './day03_input.txt';
 const data = fs.readFileSync(filePath, 'utf8');
 
 const sideLengthSets = data.split('\r\n').map((line)=> line.trim().split('  ').map(Number));
-let numOfValidTriangles = 0;
-
-for (const set of sideLengthSets) {
-	const maxLenSide = Math.max(...set);
-	set.splice(set.indexOf(maxLenSide), 1);
-	const [sideA, sideB] = set;
-
-	if (sideA + sideB > maxLenSide) {
-		++numOfValidTriangles;
-	}
-}
+const numOfValidTriangles = sideLengthSets.filter(([a, b, c]) => a + b > c && b + c > a && a + c > b).length;
 
 console.log(`Answer is: ${numOfValidTriangles}`);
