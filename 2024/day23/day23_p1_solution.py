f1057c02bf0b8ffeb718d734919a1fa015b3a363
dovkh42 <doveloper42@gmail.com>
2024-12-23 23:02:10 +0200

    day 23 p1 solved

---
+++ 2024/day23/day23_p1_solution.py
@@ -0,0 +1,27 @@
+with open('input.txt', 'rt') as file:
+    pc_map = {}
+
+    for line in file:
+        a, b = line.strip().split('-')
+        if a not in pc_map:
+            pc_map[a] = []
+        if b not in pc_map:  
+            pc_map[b] = []
+
+        pc_map[a].append(b)
+        pc_map[b].append(a)
+
+result = set()
+for first, first_links in pc_map.items():
+    for second in first_links:
+        second_values = pc_map[second]
+        if len(second_values) == 0:
+            continue
+        for third in second_values:
+            if third != first and third in first_links and any(str.startswith('t') for str in [first, second, third]):
+                result.add((first, second, third))
+    pc_map[first].clear()
+
+unique_combinations = {frozenset(triplet) for triplet in result}
+
+print(len(unique_combinations))
